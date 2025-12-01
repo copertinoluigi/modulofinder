7325953c55b8cb9846b98deefae9c86c515c4964
copertinoluigi <copertino.luigi@gmail.com>
2025-12-01 18:11:00 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'm.media-amazon.com' },
-      { protocol: 'https', hostname: 'images-na.ssl-images-amazon.com' }
+      { protocol: 'https', hostname: 'images-na.ssl-images-amazon.com' },
+      { protocol: 'https', hostname: 'images-eu.ssl-images-amazon.com' }
     ],
   },
 };
